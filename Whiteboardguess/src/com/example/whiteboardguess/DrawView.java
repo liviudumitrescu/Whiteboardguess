43acc9f8654d73e4338b4753f6a1b2b5a22b6ce1
liviudumitrescu <liviu.dumitrescu@cti.pub.ro>
2014-03-07 22:00:21 +0200

    Create login screen and use Parse service. Create Drawing  activity

---
+++ Whiteboardguess/src/com/example/whiteboardguess/DrawView.java
@@ -0,0 +1,97 @@
+package com.example.whiteboardguess;
+
+
+import android.content.Context;
+import android.graphics.*;
+import android.view.*;
+import android.widget.*;
+
+
+public class DrawView extends View {
+	
+	LinearLayout layout;
+
+	TextView textView;
+
+	private Bitmap  mBitmap;
+	private Canvas  mCanvas;
+	private Path    mPath;
+	private Paint   mBitmapPaint;
+	private Paint   mPaint;
+	
+	Context context;
+
+	public DrawView(Context c, Paint mPaint) {
+	super(c);
+	this.mPaint = mPaint;
+	context=c;
+	mPath = new Path();
+	mBitmapPaint = new Paint(Paint.DITHER_FLAG);	
+	
+	}
+	public void colorChanged(int color) {
+		mPaint.setColor(color);
+		}
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+	super.onSizeChanged(w, h, oldw, oldh);
+	mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+	mCanvas = new Canvas(mBitmap);
+	
+	}
+	@Override
+	protected void onDraw(Canvas canvas) {
+	super.onDraw(canvas);
+	canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+	canvas.drawPath(mPath, mPaint);
+	
+	}
+
+	private float mX, mY;
+	private static final float TOUCH_TOLERANCE = 4;
+
+	private void touch_start(float x, float y) {
+	mPath.reset();
+	mPath.moveTo(x, y);
+	mX = x;
+	mY = y;
+	}
+	private void touch_move(float x, float y) {
+	float dx = Math.abs(x - mX);
+	float dy = Math.abs(y - mY);
+	if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+	    mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+	    mX = x;
+	    mY = y;
+	}
+	}
+	private void touch_up() {
+	mPath.lineTo(mX, mY);
+	mCanvas.drawPath(mPath, mPaint);
+	mPath.reset();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+	float x = event.getX();
+	float y = event.getY();
+
+	switch (event.getAction()) {
+	    case MotionEvent.ACTION_DOWN:
+	        touch_start(x, y);
+	        invalidate();
+	        break;
+	    case MotionEvent.ACTION_MOVE:
+	        touch_move(x, y);
+	        invalidate();
+	        break;
+	    case MotionEvent.ACTION_UP:
+	        touch_up();
+	        invalidate();
+	        break;
+	}
+	return true;
+	}  
+	
+}
+
--- Whiteboardguess/src/com/example/whiteboardguess/MainActivity.java
+++ Whiteboardguess/src/com/example/whiteboardguess/MainActivity.java
@@ -1,23 +1,74 @@
 package com.example.whiteboardguess;
 
+
 import android.os.Bundle;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.view.Menu;
-
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+import android.view.MenuItem;
+import android.graphics.*;
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+
+public class MainActivity extends Activity implements ColorPickerDialog.OnColorChangedListener {
+	DrawView dv ;   
+	AlertDialog dialog;
+	private static final int COLOR_MENU_ID = Menu.FIRST;	
+	private static final int ERASE_MENU_ID = Menu.FIRST + 1;
+	private Paint   mPaint;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {		
+		mPaint = new Paint();
+		mPaint.setAntiAlias(true);
+		mPaint.setDither(true);
+		mPaint.setColor(0xFFFF0000);
+		mPaint.setStyle(Paint.Style.STROKE);
+		mPaint.setStrokeJoin(Paint.Join.ROUND);
+		mPaint.setStrokeCap(Paint.Cap.ROUND);
+		mPaint.setStrokeWidth(20);
+		super.onCreate(savedInstanceState);
+	    dv = new DrawView(this, this.mPaint);
+	    dv.setDrawingCacheEnabled(true);
+	    setContentView(dv);
+	   
+	    
+	}
+	public void colorChanged(int color) {
+		mPaint.setColor(color);
+		}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	super.onCreateOptionsMenu(menu);
+
+	menu.add(0, COLOR_MENU_ID, 0, "Color");
+	menu.add(0, ERASE_MENU_ID, 0, "Erase");
+	
+	return true;
+	}
+
+	@Override
+	 public boolean onPrepareOptionsMenu(Menu menu) {
+	 super.onPrepareOptionsMenu(menu);
+	 return true;
+	 }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	mPaint.setAlpha(0xFF);
+
+	switch (item.getItemId()) {
+	case COLOR_MENU_ID:
+	    new ColorPickerDialog(this, this, mPaint.getColor()).show();
+	    return true;
+	case ERASE_MENU_ID:
+	   mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+	   mPaint.setAlpha(0x80);
+	   return true;
+	
+	}
+	return super.onOptionsItemSelected(item);
+	}
 }
